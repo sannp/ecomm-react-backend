ca413be3fea2b9b218ff4838fa02ffe9243f42a3
Sanket <25435438+sannp@users.noreply.github.com>
2020-04-25 14:49:26 +0530

    Connected to mongoDB

--- server.js
+++ server.js
@@ -2,7 +2,41 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
-let Phone = require('./models/phones.model');
+const jwt = require("express-jwt"); // Validate JWT and set req.user
+const jwksRsa = require("jwks-rsa"); // Retrieve RSA keys from a JSON Web Key set (JWKS) endpoint
+const checkScope = require("express-jwt-authz"); // Validate JWT scopes
+const Phone = require('./models/phones.model');
+
+/******** Authentication Functions *******/
+const checkJwt = jwt({
+  // Dynamically provide a signing key based on the kid in the header
+  // and the signing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true, // cache the signing key
+    rateLimit: true,
+    jwksRequestsPerMinute: 5, // prevent attackers from requesting more than 5 per minute
+    jwksUri: `https://${
+      process.env.REACT_APP_AUTH0_DOMAIN
+    }/.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer.
+  audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+  issuer: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/`,
+
+  // This must match the algorithm selected in the Auth0 dashboard under your app's advanced settings under the OAuth tab
+  algorithms: ["RS256"]
+});
+function checkRole(role) {
+    return function(req, res, next) {
+        const assignedRoles = req.user["http://localhost:3000/roles"];
+        if (Array.isArray(assignedRoles) && assignedRoles.includes(role)) {
+        return next();
+        } else {
+        return res.status(401).send("Insufficient role");
+        }
+    };
+}
 
 const app = express();
 
@@ -12,6 +46,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+/******** MongoDB Connection *******/
 //----Use the url if available
 mongoose.connect(process.env.URL || "mongodb://admin:admins1@ds217099.mlab.com:17099/phone-store", { useNewUrlParser: true, useCreateIndex: true });
 const connection = mongoose.connection;
@@ -22,11 +57,40 @@ connection.once('open', () => { console.log("MongoDB database connection establi
 //Bind connection to error event (to get notification of connection errors)
 connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-//If application is in production
+/******** Routes *******/
 app.get('/', function (req, res) {
     Phone.find().then( phones => res.json(phones));
 });
 
+app.get("/public", function(req, res) {
+    res.json({
+        message: "Hello from a public API!"
+    });
+});
+
+app.get("/private", checkJwt, function(req, res) {
+    res.json({
+        message: "Hello from a private API!"
+    });
+});
+
+app.get("/course", checkJwt, checkScope(["read:courses"]), function(req, res) {
+    res.json({
+        courses: [
+        { id: 1, title: "Building Apps with React and Redux" },
+        { id: 2, title: "Creating Reusable React Components" }
+        ]
+    });
+});
+
+
+app.get("/admin", checkJwt, checkRole("admin"), function(req, res) {
+res.json({
+    message: "Hello from an admin API!"
+});
+});
+  
+/******** Start Server *******/
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}` );
 });
